fa11842d5026e4be462d6ba56f521c0306a65189
Mohammed-Ahmed-Ezzat <mohammedahmedezzat.2004@gmail.com>
2026-09-22 06:17:26 +0300

    setup github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/task-1-react">
       <App />
     </BrowserRouter>
   </StrictMode>,
