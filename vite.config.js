43f0623cd14e9de203b674caa77eb2f1da8e7e8f
Mohammed-Ahmed-Ezzat <mohammedahmedezzat.2004@gmail.com>
2026-09-22 06:27:38 +0300

    handle spa 404 on github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import fs from 'node:fs'
+import path from 'node:path'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        fs.copyFileSync(
+          path.resolve('dist/index.html'),
+          path.resolve('dist/404.html')
+        )
+      }
+    }
+  ],
   base: '/task-1-react/',
 })
